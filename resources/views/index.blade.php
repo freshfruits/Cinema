79e0725924b1da87e664e782c54a1bdadaaab001
Freshfruits <freshfruits12@protonmail.ch>
2018-03-23 09:32:56 +0100

    Auth on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,19 @@
 @extends('templates.pages.generic')
-
 @section('content')
+@if (Auth::check())
 <div class="card">
-  <div class="card-body">
-    Velkommen
-  </div>
+   <div class="card-body">
+        <h2>Hey {{ Auth::user()->username }}</h2>
+        <hr>
+        <p>We hope you have a great time here and get to watch a lot of awesome movies!</p>
+   </div>
 </div>
-
+@else 
+<div class="card">
+    <div class="card-body">
+        <h2>Welcome Guest</h2>
+        <hr>
+        <p>Please <a href="{{ route('login') }}">login</a> to see all the awesome movies!</p>
+</div>
+@endif
 @endsection
