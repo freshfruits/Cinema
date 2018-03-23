c1d43971b6a6c3442f5b827f6f32753872a7d34f
Freshfruits <freshfruits12@protonmail.ch>
2018-03-23 07:29:21 +0100

    Fix Username login & Role on registration

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,10 +46,10 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    /*
+    
     public function username() 
     {
         return 'username';
     }
-    */
+    
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,11 +63,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+        $user->assignRole('regular');
+        return $user;
     }
 }
